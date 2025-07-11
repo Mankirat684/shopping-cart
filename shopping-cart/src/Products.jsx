0945017a96636044b19dfc444eec3f3474fdb453
Mankirat684 <mankirats213@gmail.com>
2025-07-11 17:11:27 +0530

    added products rendering logic

--- shopping-cart/src/Products.jsx
+++ shopping-cart/src/Products.jsx
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 
 function Products() {
-    useEffect(()=>{
-        fetch("https://fakestoreapi.com/products")
-        .then((response) => response.json)
-        .then((data) => console.log(data))
-    },[]
+  let [productsArr, setproductsArr] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setproductsArr(data);
+      });
+  }, []);
+  let RenderArr = productsArr.map((products,index) => {
+    return (
+      <div className="product" key={index}>
+        <p>{products.title}</p>
+        <img src={products.image} alt={products.title} />
+        <p className="price">{products.price}</p>
+      </div>
     );
-  
+  });
+  return(
+    <>
+    {RenderArr}
+    </>
+  )
 }
 export default Products;
--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import Products from "./Products.jsx";
 
 let routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/products",
+    element: <Products />
   },
 ]);
 createRoot(document.getElementById("root")).render(
